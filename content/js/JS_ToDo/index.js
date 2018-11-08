26a9157262d89fa80f55b19893f91b85bc100657
webDevWay <k.vasiliy.r@gmail.com>
2018-11-08 22:11:06 +0800

    change ES6 ==> ES5

--- content/js/JS_ToDo/index.js
+++ content/js/JS_ToDo/index.js
@@ -1,33 +1,35 @@
-const buttonAdd = document.getElementById('add');
-const mainUl = document.getElementById('mainUl');
-let newTask = document.getElementById('newTask');
-const date = new Date();
+'use strict';
+
+var buttonAdd = document.getElementById('add');
+var mainUl = document.getElementById('mainUl');
+var newTask = document.getElementById('newTask');
+var date = new Date();
 
 function createItem(task) {
-    if(newTask.value) {
-            const li = document.createElement('li');
-            const buttonDel = document.createElement('i');
-            buttonDel.className = "fas fa-trash-alt delete";
-            const spanText = document.createElement('span');
-            spanText.innerHTML = newTask.value;
-            const br = document.createElement('br');
-            const spanDate = document.createElement('span');
-            spanDate.className = "date";
-            spanDate.innerHTML = date.toLocaleString('ru', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'});
-            mainUl.appendChild(li);
-            li.appendChild(spanText);
-            li.appendChild(buttonDel);
-            li.appendChild(br);
-            li.appendChild(spanDate);
+    if (newTask.value) {
+        var li = document.createElement('li');
+        var buttonDel = document.createElement('i');
+        buttonDel.className = "fas fa-trash-alt delete";
+        var spanText = document.createElement('span');
+        spanText.innerHTML = newTask.value;
+        var br = document.createElement('br');
+        var spanDate = document.createElement('span');
+        spanDate.className = "date";
+        spanDate.innerHTML = date.toLocaleString('ru', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
+        mainUl.appendChild(li);
+        li.appendChild(spanText);
+        li.appendChild(buttonDel);
+        li.appendChild(br);
+        li.appendChild(spanDate);
 
-            buttonDel.addEventListener('click', removeItem);
-            newTask.value = "";
-            return li;
-        }
+        buttonDel.addEventListener('click', removeItem);
+        newTask.value = "";
+        return li;
+    }
     save();
 }
-function addItem () {
-    const li = createItem(newTask.value);
+function addItem() {
+    var li = createItem(newTask.value);
 }
 buttonAdd.onclick = addItem;
 function removeItem() {
