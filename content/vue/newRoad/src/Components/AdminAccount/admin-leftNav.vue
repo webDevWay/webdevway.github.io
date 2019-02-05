bad38b83b1fc005c65fedd27a43b2fb0284ba508
webdevway <k.vasiliy.@gmail.com>
2019-02-05 12:03:27 +0800

    newRoad

---
+++ content/vue/newRoad/src/Components/AdminAccount/admin-leftNav.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="col-md-2 col-sm-12 p-0">
+        <ul>
+            <li class="nav-list-category ">GENERAL</li>
+
+            <li class="nav-list-item "><a href="#"><i class="fas fa-user-circle fasicons"></i>Dashboard</a></li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-paint-brush fasicons"></i>Layouts</a></li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-mail-forward fasicons"></i>Pages</a></li>
+            <li class="nav-list-category">ELEMENTS</li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-recycle fasicons"></i>Важное</a></li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-home fasicons"></i>Результаты</a></li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-user-circle fasicons"></i>О нас</a></li>
+            <li class="nav-list-item "><a href="#"><i class="fas fa-address-book fasicons"></i>Контакты</a></li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'acc-leftNav',
+    }
+</script>
+
+
+<style lang="sass">
+
+    ul
+        padding: 20px 0 0 0
+        /*text-align: center*/
+
+        a
+            text-decoration: none
+            color: black
+            display: block
+            padding: 3% 5% 3% 10%
+        a:hover
+            color: black
+            text-decoration: none
+        .nav-list-category
+            padding: 3% 5% 3% 10%
+            color: #757575
+        .fasicons
+            margin-right: 7px
+            color: #757575
+
+
+        .nav-list-item
+            list-style-type: none
+            width: 100%
+            color: #424242
+        .nav-list-item:hover
+            background: #F1F4F5
+
+
+
+</style>
---
+++ content/vue/newRoad/src/Components/Adminaccount.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="account">
+        <acc-header></acc-header>
+        <!--  Nav + Main -->
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Fixed Left Nav -->
+                <acc-leftNav></acc-leftNav>
+                <!-- Main -->
+                <!-- First Row -->
+                <acc-main></acc-main>
+                <!-- /Cards -->
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import store from '../Store/store.js'
+    import accHeader from "./AdminAccount/admin-header.vue";
+    import accLeftNav from "./AdminAccount/admin-leftNav.vue";
+    import accMain from "./AdminAccount/admin-main.vue";
+
+    export default {
+        components: {
+            accHeader,
+            accLeftNav,
+            accMain,
+        },
+        name: 'admin-account',
+    }
+</script>
+
+
+<style lang="sass">
+
+
+</style>
