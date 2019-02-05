bf1e4de14e4f37d105b9f74cf1fdc7fff70e1743
webdevway <k.vasiliy.@gmail.com>
2019-02-05 12:00:51 +0800

    newRoad

--- content/vue/newRoad/app - JKH/src/Firebase/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyAD_tnNHwsRPz3tt0Dn3e1dWcWraUWQSFY",
-    authDomain: "testnewroad.firebaseapp.com",
-    databaseURL: "https://testnewroad.firebaseio.com",
-    projectId: "testnewroad",
-    storageBucket: "testnewroad.appspot.com",
-    messagingSenderId: "681769695460"
-}
