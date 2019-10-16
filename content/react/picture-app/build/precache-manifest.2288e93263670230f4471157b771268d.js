d3ab56bc2425df6b3733ddc6471db01f200edc0b
webdevway <k.vasiliy.@gmail.com>
2019-10-16 13:04:25 +0800

    build

---
+++ content/react/picture-app/build/precache-manifest.2288e93263670230f4471157b771268d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512614b652589bdb04c08739736dcf56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07db3c598b0f46f4a8e6",
+    "url": "/static/css/main.5c9ca218.chunk.css"
+  },
+  {
+    "revision": "6761d1e1e799e28b2ed2",
+    "url": "/static/js/2.14fdd220.chunk.js"
+  },
+  {
+    "revision": "07db3c598b0f46f4a8e6",
+    "url": "/static/js/main.a8a0d389.chunk.js"
+  },
+  {
+    "revision": "8d492a998d0464684096",
+    "url": "/static/js/runtime-main.18ee6681.js"
+  }
+]);
