6f1de5b592b22e812fbc68946db764cc46cf0a26
webdevway <k.vasiliy.@gmail.com>
2019-11-30 22:03:44 +0800

    www

--- content/www/DanceColor/script.js
+++ content/www/DanceColor/script.js
@@ -1,9 +1,20 @@
-$('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  variableWidth: true,
-  easing:	'linear',
-  appendArrows: $('.slider_btn'),
-  prevArrow: '<span id="slider_btn_prev" class="slider_btn_left"><i class="fa fa-arrow-left" aria-hidden="true"></i></span>',
-  nextArrow: '<span id="slider_btn_next" class="slider_btn_right"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>'
-});
+window.onload = function () {
+  //burger - menu show / hide
+  let burger = document.querySelector(".main-nav-burger-wrapper");
+  let burgerMenu = document.querySelector('.main-nav-burger');
+  let burgerLine = document.querySelector(".main-nav-burger_line");
+  burger.addEventListener('click', function () {
+    console.log(burger, burgerMenu, burgerLine);
+    burgerMenu.classList.toggle('hidden');
+    burgerLine.classList.toggle('burger_transform');
+  });
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    easing: 'linear',
+    appendArrows: $('.slider_btn'),
+    prevArrow: '<span id="slider_btn_prev" class="slider_btn_left"><i class="fa fa-arrow-left" aria-hidden="true"></i></span>',
+    nextArrow: '<span id="slider_btn_next" class="slider_btn_right"><i class="fa fa-arrow-right" aria-hidden="true"></i></span>'
+  });
+};
